00f72d917bcb3e817c973795872c548f9d6c839f
jananinarayanasamy <janani.narayanasamy@hcl.com>
2020-03-11 17:41:34 +0530

    Changes made for raw data

--- candidatesList.php
+++ candidatesList.php
@@ -1,10 +1,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json;");
+$json = file_get_contents('php://input');
+$data = json_decode($json,true);
+
 
 require_once 'include/dbconnect.php';
-$event_ID = $_REQUEST['event_id'];
-    
+ $event_ID = $data['event_id'];
+
 $query = "select cr.EmpID,cr.EmpName,sk.Skills,cr.Expereince,cr.RelevantExperience,cr.ContactNo,DATE(re.Date) as date from candidate_event ce left join candidate_registration cr on cr.ID = ce.CanidateID left join register_event re on re.EventID = ce.EventID left join skills sk on sk.SkillId = cr.Skills where ce.EventID=$event_ID";
 $result = mysqli_query($conn,$query);
 $candidates = [];
