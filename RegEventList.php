6f60d4dac09d8c816e196494ec82f2b09bd45619
jananinarayanasamy <janani.narayanasamy@hcl.com>
2020-03-18 13:32:28 +0530

    query changes

--- RegEventList.php
+++ RegEventList.php
@@ -3,7 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once 'include/dbconnect.php';
-$query = "SELECT a.EventName as Name ,a.Date as EventDate, GROUP_CONCAT(DISTINCT b.skills) SkillName,c.ClientName as ClientName,GROUP_CONCAT(DISTINCT d.AssementScaleName) AssessmentScaleName, e.Duration as Duration,IF(ProblemSolvingSkillTested=1,'TRUE','FALSE') as PSSkillTest ,IF(TechnicalSkilslsTested=1,'TRUE','FALSE') as TechSkillTested , IF(CommunicationSkillIsTested=1,'TRUE','FALSE') as ComSkillTested,IF(LogicalSkillsIsTested=1,'TRUE','FALSE') as LogSkillTested FROM `register_event` a INNER JOIN skills b ON FIND_IN_SET(b.SkillId, a.`Skills`) INNER JOIN `client` c on a.`Skills` = c.`ClientId` INNER JOIN `assessmentscale` d ON FIND_IN_SET(d.`AssessmentId` , a.`AssessmentScale`) INNER JOIN `duration` e on a.`Duration`= e.`DurationID` GROUP BY a.EventID ";
+//$query = "SELECT a.EventName as Name ,a.Date as EventDate, GROUP_CONCAT(DISTINCT b.skills) SkillName,c.ClientName as ClientName,GROUP_CONCAT(DISTINCT d.AssementScaleName) AssessmentScaleName, e.Duration as Duration,IF(ProblemSolvingSkillTested=1,'TRUE','FALSE') as PSSkillTest ,IF(TechnicalSkilslsTested=1,'TRUE','FALSE') as TechSkillTested , IF(CommunicationSkillIsTested=1,'TRUE','FALSE') as ComSkillTested,IF(LogicalSkillsIsTested=1,'TRUE','FALSE') as LogSkillTested FROM `register_event` a INNER JOIN skills b ON FIND_IN_SET(b.SkillId, a.`Skills`) INNER JOIN `client` c on a.`Skills` = c.`ClientId` INNER JOIN `assessmentscale` d ON FIND_IN_SET(d.`AssessmentId` , a.`AssessmentScale`) INNER JOIN `duration` e on a.`Duration`= e.`DurationID` GROUP BY a.EventID ";
+$query = "SELECT EventID as EventId , EventName as Name ,Date as EventDate FROM `register_event` where isClosed='0' ";
+
 $result = mysqli_query($conn,$query);
 $durationset = array();
 if(mysqli_num_rows($result) > 0){
